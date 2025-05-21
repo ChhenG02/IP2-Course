6411a03d17fd4f7541066ecba846c13ec239784b
ChhenG02 <itcdev@edu.kh>
2025-05-21 13:17:53 +0700

    fetching and create tasks with api

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         >You have <span class="pending-num"> {{ nbOfTodo }} </span> tasks
         pending.</span
       >
-      <button class="clear-button">Clear All</button>
+      <button class="clear-button" @click="clearAllTodos">Clear All</button>
     </div>
   </div>
 </template>
@@ -40,7 +40,7 @@ export default {
   },
   methods: {
     handleAddTodo(todo) {
-      this.store.addTodo(todo);
+      this.store.createTodo(todo);
     },
     clearAllTodos() {
       console.log("clear");
--- src/stores/todo.js
+++ src/stores/todo.js
@@ -1,57 +1,59 @@
+// src/stores/todo.js
 import { defineStore } from "pinia";
+import axios from "axios";
+
+const API = "http://localhost:3100/tasks";
 
 export const useTodoStore = defineStore("todo", {
   state: () => ({
     todos: [],
   }),
-  getters: {
-    countTodos: (state) => state.todos.length,
-  },
+
   actions: {
     async fetchTodos() {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve([
-            {
-              id: 1,
-              name: "Clean house",
-              description: "cleaning house in detail .....",
-              createdAt: "2024-15-07 07:50:00",
-              completedAt: null,
-            },
-            {
-              id: 2,
-              name: "Do homework",
-              description: "Instruction on doing homework ....",
-              createdAt: "2024-05-07 08:00:00",
-              completedAt: "2024-05-07 08:10:00",
-            },
-          ]);
-        }, 1000);
-      }).then((todos) => (this.todos = todos));
-    },
-    toggleStatus(id) {
-      const foundIndex = this.todos.findIndex((t) => t.id == id);
-      if (foundIndex >= 0) {
-        if (this.todos[foundIndex].completedAt != null) {
-          this.todos[foundIndex].completedAt = null;
-        } else {
-          this.todos[foundIndex].completedAt = new Date().toISOString();
-        }
+      try {
+        const res = await axios.get(API);
+        this.todos = res.data;
+      } catch (err) {
+        console.error("Failed to fetch todos:", err);
       }
     },
-    addTodo(todo) {
-      this.todos.push({
-        id: this.todos.length + 1,
-        name: todo,
-        description: "description",
-        createdAt: new Date().toISOString(),
-        completedAt: null,
-      });
-      this.todos = JSON.parse(JSON.stringify(this.todos));
+
+    async createTodo(taskName) {
+      try {
+        const taskData = {
+          name: taskName,
+          description: null,
+          completedAt: null,
+          userId: 1,
+        };
+
+        const res = await axios.post(`${API}/create`, taskData);
+        this.todos.push(res.data);
+      } catch (err) {
+        console.error("Failed to create todo:", err);
+        throw err;
+      }
     },
-    clearAll() {
-      this.todos = [];
+
+    async toggleStatus(id) {
+      try {
+        const todo = this.todos.find((t) => t.id === id);
+        const updatedData = {
+          completedAt: todo.completedAt ? null : new Date().toISOString(),
+        };
+
+        await axios.put(`${API}/${id}`, updatedData);
+        await this.fetchTodos();
+      } catch (err) {
+        console.error("Failed to toggle todo status:", err);
+      }
+    },
+  },
+
+  getters: {
+    countTodos() {
+      return this.todos.filter((todo) => !todo.completedAt).length;
     },
   },
 });
